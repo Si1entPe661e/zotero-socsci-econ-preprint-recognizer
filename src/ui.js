278ce0ff4ad240be90f899f1bb6b6fa518fba303
Si1entPe661e <Si1entPe661e@gmail.com>
2026-06-02 16:29:02 +0800

    fix: provide UI module load target

---
+++ src/ui.js
@@ -0,0 +1,10 @@
+(function (root, factory) {
+  if (typeof module === "object" && module.exports) {
+    module.exports = factory();
+  } else {
+    root.NBERZotero = root.NBERZotero || {};
+    Object.assign(root.NBERZotero, factory());
+  }
+})(typeof globalThis !== "undefined" ? globalThis : this, function () {
+  return {};
+});
